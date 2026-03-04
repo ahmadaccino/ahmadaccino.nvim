6cc3439aab57496efda76e149e893c0e03cd7fa0
ahmadaccino <ahmad@ndstudio.gov>
2026-03-04 13:15:28 -0500

    images and more

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,6 +5,27 @@
 
 return {
   {
+    '3rd/image.nvim',
+    lazy = false,
+    build = false, -- disable luarocks rock install; magick_cli processor doesn't need it
+    opts = {
+      backend = 'kitty',
+      processor = 'magick_cli',
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { 'markdown', 'vimwiki' },
+        },
+      },
+      max_width_window_percentage = 80,
+      max_height_window_percentage = 50,
+      hijack_file_patterns = { '*.png', '*.jpg', '*.jpeg', '*.gif', '*.webp', '*.avif' },
+    },
+  },
+  {
     'supermaven-inc/supermaven-nvim',
     config = function()
       require('supermaven-nvim').setup {
@@ -31,4 +52,14 @@ return {
     end,
   },
   { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = function() end },
+  {
+    'taigrr/blast.nvim',
+    event = 'VeryLazy',
+    opts = {},
+  },
+  {
+    'taigrr/neocrush.nvim',
+    event = 'VeryLazy',
+    opts = {},
+  },
 }
